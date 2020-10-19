9d9124f5ea45db911aa735d23fab7c8093230680
Markus Bressel <mbressel@eggs.de>
2020-10-19 15:42:35 +0200

    .

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import {Nav, Navbar, Container} from 'react-bootstrap';
 
+import {
+    BrowserRouter, Link
+} from 'react-router-dom';
+
 const MainNavigation = () => {
     return (
         <div>
             <Navbar bg="dark" variant="dark" fixed="top">
                 <Container>
-                    <Navbar.Brand href="/">Dashboard</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/tasklist">Offene Tasks</Nav.Link>
-                        <Nav.Link href="/checklist">Checklisten</Nav.Link>
-                        <Nav.Link href="/kackundsach">Kack</Nav.Link>
-                        <Nav.Link href="/dummy">Dummy</Nav.Link>
-                    </Nav>
+                    <BrowserRouter>
+                        <Navbar.Brand href="/">Dashboard</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Link to="/tasklist">Offene Tasks</Link>
+                            <Nav.Link href="/tasklist">Offene Tasks</Nav.Link>
+                            <Nav.Link href="/checklist">Checklisten</Nav.Link>
+                            <Nav.Link href="/kackundsach">Kack</Nav.Link>
+                            <Nav.Link href="/dummy">Dummy</Nav.Link>
+                        </Nav>
+                    </BrowserRouter>
                 </Container>
             </Navbar>
         </div>
