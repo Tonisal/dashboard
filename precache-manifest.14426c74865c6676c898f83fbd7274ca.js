45794d5362a29e662e79e7d31969f1bf105a7619
Markus Bressel <mbressel@eggs.de>
2020-10-19 15:25:42 +0200

    Updates

---
+++ precache-manifest.14426c74865c6676c898f83fbd7274ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96c6145661f861d217bfb912689a6bb",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "46db8a10a8a879bda3a3",
+    "url": "/dashboard/static/css/main.a577e3d8.chunk.css"
+  },
+  {
+    "revision": "d14964bfe1fb081b3d77",
+    "url": "/dashboard/static/js/2.2311dc57.chunk.js"
+  },
+  {
+    "revision": "fe55b5862f2aaaa5c6f2c8ef67ae80f1",
+    "url": "/dashboard/static/js/2.2311dc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46db8a10a8a879bda3a3",
+    "url": "/dashboard/static/js/main.aeb660f1.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  },
+  {
+    "revision": "bb05e131d35ec37ef08eb37d3e453f8b",
+    "url": "/dashboard/static/media/arrow.bb05e131.svg"
+  }
+]);
