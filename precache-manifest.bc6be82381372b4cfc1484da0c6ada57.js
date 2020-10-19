46cf910b99bd01fde2b9c53b2ba7a29ab2aa53aa
Markus Bressel <mbressel@eggs.de>
2020-10-19 15:44:49 +0200

    Updates

---
+++ precache-manifest.bc6be82381372b4cfc1484da0c6ada57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0d973a7f1c87ca4062733ad52c12e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "663e71f907a1b7797e72",
+    "url": "/static/css/main.a577e3d8.chunk.css"
+  },
+  {
+    "revision": "5ab19d857c3883de19a1",
+    "url": "/static/js/2.cc83ea03.chunk.js"
+  },
+  {
+    "revision": "fe55b5862f2aaaa5c6f2c8ef67ae80f1",
+    "url": "/static/js/2.cc83ea03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663e71f907a1b7797e72",
+    "url": "/static/js/main.bc477246.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  },
+  {
+    "revision": "bb05e131d35ec37ef08eb37d3e453f8b",
+    "url": "/static/media/arrow.bb05e131.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.1aad26f0271878bb6fa9c962b8007c62.js"
+  "/precache-manifest.bc6be82381372b4cfc1484da0c6ada57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
